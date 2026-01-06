b5d6ce0b7e5aeab892a7163b0d5f8d640a5f6a9b
s3nnin-mode <159660271+s3nnin-mode@users.noreply.github.com>
2026-01-06 11:44:06 -0800

    cambiar vista de cada proyecto

--- src/componentes/proyecto.jsx
+++ src/componentes/proyecto.jsx
@@ -1,6 +1,49 @@
 import { useRef, useState } from 'react';
 import '../stylesheet/proyecto.scss';
 
+// export const Proyecto = ({nombreProyecto, img, link, detalles}) => {
+//   const [detallesAbierto, setDetallesAbierto] = useState(false);
+//   const refCollapse = useRef(null);
+
+//   const handleCollapse = () => {
+//     setDetallesAbierto(!detallesAbierto);
+//     const isDetallesAbierto = !detallesAbierto;
+//     const detalles = refCollapse.current;
+
+//     if (isDetallesAbierto) {
+//       detalles.style.gridTemplateRows = '1fr'
+//     } else {
+//       detalles.style.gridTemplateRows = '0fr';
+//     }
+//   }
+
+//   return (
+//     <div className='proyecto'>
+//       <p className='nombre-proyecto montserrat'>{nombreProyecto}</p>
+//       <img src={img} alt={img} className='img_proyecto' />
+//       {/* <span className='link-proyecto'><a href='https://weather-app-re-gilt.vercel.app/'>{link}</a></span> */}
+      
+//       <div className='btns-proyecto'>
+//         <button 
+//         className={`montserrat btn-detalle ${detallesAbierto ? 'detalles-abierto' : 'detalles-cerrado'}`} 
+//         onClick={handleCollapse}>
+
+//         {detallesAbierto ? 'Cerrar' : 'ver detalles'}
+
+//         </button>
+//         <a className='montserrat' href={link} target='_blank' rel='noreferrer'>
+//           <span>Ir al proyecto</span>
+//           <img src='/imgs/iconos/icon-portafolio.svg' alt="" />
+//         </a>
+//       </div>
+      
+//       <div className='detalles' ref={refCollapse}>
+//         <div className='inner'>{detalles}</div>
+//       </div>
+//     </div>
+//   )
+// }
+
 export const Proyecto = ({nombreProyecto, img, link, detalles}) => {
   const [detallesAbierto, setDetallesAbierto] = useState(false);
   const refCollapse = useRef(null);
@@ -18,28 +61,18 @@ export const Proyecto = ({nombreProyecto, img, link, detalles}) => {
   }
 
   return (
-    <div className='proyecto'>
-      <p className='nombre-proyecto montserrat'>{nombreProyecto}</p>
-      <img src={img} alt={img} className='img_proyecto' />
-      {/* <span className='link-proyecto'><a href='https://weather-app-re-gilt.vercel.app/'>{link}</a></span> */}
-      
-      <div className='btns-proyecto'>
-        <button 
-        className={`montserrat btn-detalle ${detallesAbierto ? 'detalles-abierto' : 'detalles-cerrado'}`} 
-        onClick={handleCollapse}>
-
-        {detallesAbierto ? 'Cerrar' : 'ver detalles'}
-
-        </button>
-        <a className='montserrat' href={link} target='_blank' rel='noreferrer'>
-          <span>Ir al proyecto</span>
-          <img src='/imgs/iconos/icon-portafolio.svg' alt="" />
-        </a>
+    <article className='proyecto'>
+      <div className='text'>
+        <h1 className='titulo'>{nombreProyecto}</h1>
+        <div className='descripcion'>
+          {detalles}
+        </div>
       </div>
-      
-      <div className='detalles' ref={refCollapse}>
-        <div className='inner'>{detalles}</div>
+      <div className='img-wrapper'>
+        <img 
+          src={img}
+        />
       </div>
-    </div>
+    </article>
   )
 }
